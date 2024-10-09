b3870a3468decd75661db61867f512c562b4f847
Chrizelda03 <chrizeldanorial@gmail.com>
2024-10-09 16:06:39 +0800

    Problem 4.php

---
+++ Problem 4.php
@@ -0,0 +1,19 @@
+<?php
+class Vehicle {
+    public $name;
+    public $speed;
+    public $mileage;
+
+    public function __construct($name, $speed, $mileage) {
+        $this->name = $name;
+        $this->speed = $speed;
+        $this->mileage = $mileage;
+    }
+}
+class Bus extends Vehicle {
+  public function seating_capacity() {
+    $seating_capacity = 50;
+    return $seating_capacity;
+  }
+}
+?>
