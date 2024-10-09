827cc5e6c8023aec32aea809e835f00c97094a3c
Chrizelda03 <chrizeldanorial@gmail.com>
2024-10-09 16:31:58 +0800

    Update Problem 3.php

--- Problem 3.php
+++ Problem 3.php
@@ -9,11 +9,6 @@ class Vehicle {
         $this->speed = $speed;
         $this->mileage = $mileage;
     }
-public function displayInfo() {
-        echo "Vehicle Name: " . $this->name . "\n";
-        echo "Speed: " . $this->speed . " km/h\n";
-        echo "Mileage: " . $this->mileage . " km/l\n";
-    }
 }
 class Bus extends Vehicle {
 }
